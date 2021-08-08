01551a2cf8153172e027571726f69b3342ac883a
Brandon Currell <45363644+BCurrell@users.noreply.github.com>
2021-08-08 18:59:48 +0100

    Update logic, DRY some stuff, prepare for variables

--- src/main/java/com/notoriousdev/chatping/ChatPing.java
+++ src/main/java/com/notoriousdev/chatping/ChatPing.java
@@ -13,6 +13,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class ChatPing extends JavaPlugin implements Listener
 {
 
+    // TODO: Turn these into configurable options
+    private static final Sound PING_SOUND = Sound.ENTITY_EXPERIENCE_ORB_PICKUP;
+    private static final int PING_VOLUME = 20;
+    private static final int PING_PITCH = 1;
+    private static final long PING_DELAY = 5;
+    private static final int PING_COUNT = 2;
+
     @Override
     public void onDisable()
     {
@@ -24,25 +31,59 @@ public class ChatPing extends JavaPlugin implements Listener
         getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPlayerChat(AsyncChatEvent event)
     {
-        getLogger().info("Chat event");
         Player player = event.getPlayer();
         String message = event.message().toString();
+
         if (Permissions.PING.isAuthorised(player))
         {
             for (final Player oPlayer : getServer().getOnlinePlayers())
             {
-                // oPlayer != player &&
-                if ((message.toLowerCase().contains(oPlayer.getName().toLowerCase()) || message.toLowerCase().contains(oPlayer.displayName().toString().toLowerCase())) && !Permissions.PING_EXEMPT.isAuthorised(oPlayer))
+                if (oPlayer == player || Permissions.PING_EXEMPT.isAuthorised(oPlayer))
                 {
-                    this.getLogger().info("Playing sound for " + oPlayer.getName());
-                    oPlayer.playSound(oPlayer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 20, 1);
-                    getServer().getScheduler().runTaskLater(this, () -> oPlayer.playSound(oPlayer.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 20, 1), 5L);
+                    continue;
+                }
+
+                if (this.containsIgnoreCase(message, oPlayer.getName(), oPlayer.displayName().toString()))
+                {
+                    this.playSound(iPlayer, PING_SOUND, PING_VOLUME, PING_PITCH, PING_DELAY, PING_COUNT);
                 }
             }
         }
+    }
+
+    private boolean containsIgnoreCase(String source, String... targets)
+    {
+        source = source.toLowerCase();
+
+        for (String target : targets)
+        {
+            if (source.contains(target.toLowerCase()))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void playSound(Player player, Sound sound, float volume, float pitch, long delay, int count)
+    {
+        player.playSound(player.getLocation(), sound, volume, pitch);
+
+        while (--count > 0)
+        {
+            this.getServer().getScheduler().runTaskLater(this, () -> {
+                player.playSound(player.getLocation(), sound, volume, pitch);
+            }, delay);
+        }
+    }
+
+    private void playSound(Player player, Sound sound, float volume, float pitch, long delay)
+    {
+        this.playSound(player, sound, volume, pitch, delay, PING_COUNT);
     }
 
     public enum Permissions
